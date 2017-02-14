d1f98e33118f7ce57082233d560147be142010f8
Iurii <iurii.derevianko@gmail.com>
2017-02-14 23:21:26 +0200

    Initial commit

--- lesson-04/task-05-MyTreeDSImplementations/src/ua/itea/ijavaadv/lesson04/mytreedsimpementations/Main.java
+++ lesson-04/task-05-MyTreeDSImplementations/src/ua/itea/ijavaadv/lesson04/mytreedsimpementations/Main.java
@@ -20,10 +20,13 @@ public class Main {
         MyNavigableMultiMap multiMap = new MyNavigableMultiMap();
 
 
-        System.out.println(multiMap.put(new Integer(1), new Integer[] {new Integer(1), new Integer(2)}));
-        System.out.println(multiMap.put(new Integer(1), new Integer[] {new Integer(1), new Integer(2)}));
-        System.out.println(multiMap.put(new Integer(2), new Integer[] {new Integer(1), new Integer(2), new Integer(3)}));
-        System.out.println(multiMap.put(new Integer(3), new Integer[] {new Integer(1), new Integer(2), new Integer(3), new Integer(4)}));
+        System.out.println(multiMap.put(1, new Integer[] {new Integer(1), new Integer(2)}));
+        System.out.println(multiMap.put(1, new Integer[] {new Integer(1), new Integer(2)}));
+        System.out.println(multiMap.put(2, new Integer[] {new Integer(1), new Integer(2), new Integer(3)}));
+        System.out.println(multiMap.put(3, new Integer[] {new Integer(1), new Integer(2), new Integer(3), new Integer(4)}));
         System.out.println( multiMap.size());
+
+        System.out.println("Hello my new commit");
+
     }
 }
