317bbdfdd0709f700293737fd41e1b679af3c333
Iurii <iurii.derevianko@gmail.com>
2017-02-17 23:40:43 +0200

    Initial commit

--- lesson-08/src/Main.java
+++ lesson-08/src/Main.java
@@ -1,13 +1,9 @@
-package ua.itea.ijavaadv.lesson07.cw01;
-
 /**
  * Created
- * at 18:30
- * on 16.02.17
+ * at 23:30
+ * on 17.02.17
  * by Iurii Derevianko;
- *
  */
-
 public class Main {
     public static void main(String[] args) {
 
