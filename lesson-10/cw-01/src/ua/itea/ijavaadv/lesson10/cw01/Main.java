e97fe8619e826d44d2b330d4375797a59a0d5706
Iurii <iurii.derevianko@gmail.com>
2017-02-27 20:44:29 +0200

    Initial commit

---
+++ lesson-10/cw-01/src/ua/itea/ijavaadv/lesson10/cw01/Main.java
@@ -0,0 +1,13 @@
+package ua.itea.ijavaadv.lesson10.cw01;
+
+/**
+ * Created
+ * at 19:17
+ * on 27.02.17
+ * by Iurii Derevianko;
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
