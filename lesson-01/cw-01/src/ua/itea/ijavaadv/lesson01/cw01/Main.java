85a18873692908f6db364e20a6d1e17ca5214a58
Iurii <iurii.derevianko@gmail.com>
2017-02-27 20:51:20 +0200

    Initial commit

--- lesson-01/cw-01/src/ua/itea/ijavaadv/lesson01/cw01/Main.java
+++ lesson-01/cw-01/src/ua/itea/ijavaadv/lesson01/cw01/Main.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Locale;
 
 public class Main {
+    @SuppressWarnings("all")
     public static void main(String[] args) {
         System.out.printf("%h %n", "Hello World");
         System.out.printf("%H %n", "Hello World");
@@ -12,6 +13,7 @@ public class Main {
         double real = Math.PI;
         System.out.printf(new Locale("ua", "ua"), "%20.10f %n", real);
         System.out.printf("%tC %n", new Date());
+
         System.out.printf("%s %n", new Date(2020-1900, 4-1, 1));
         System.out.printf("%s %n", new Date(-1999, 4-1, 1));
 
