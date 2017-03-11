134c4997579c92e4adf4dba3535ee0b040c0ef3d
Iurii <iurii.derevianko@gmail.com>
2017-03-11 23:45:55 +0200

    Initial commit

--- lesson-10/cw-01/src/ua/itea/ijavaadv/lesson10/cw01/Main.java
+++ lesson-10/cw-01/src/ua/itea/ijavaadv/lesson10/cw01/Main.java
@@ -1,5 +1,7 @@
 package ua.itea.ijavaadv.lesson10.cw01;
 
+import java.lang.annotation.Repeatable;
+
 /**
  * Created
  * at 19:17
@@ -7,8 +9,28 @@ package ua.itea.ijavaadv.lesson10.cw01;
  * by Iurii Derevianko;
  */
 public class Main {
+
+
     @SuppressWarnings("all")
     public static void main(String[] args) {
 
     }
+
+
+  /*  @Repeatable(Java)
+    @interface Author {
+        String name();
+        Class email();
+        Size size();
+        Info information();
+        Size[] availableSizes();
+    }
+
+    enum Size {
+        XS, S, M, L, XL, XXL
+    }
+
+    @interface Info {
+        int value();
+    }*/
 }
